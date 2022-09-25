a298d302046acad4273227bcbae9b5130ce35e1b
Leifer Mendez <leifer33@gmail.com>
2022-09-25 21:27:22 +0200

    First Publish

--- adapter/tests/qwik.js
+++ adapter/tests/qwik.js
@@ -3,6 +3,6 @@ import * as assert from "uvu/assert";
 import { QwikPartytown } from "../src";
 test("Snippet Script", () => {
     const currentSnippet = QwikPartytown();
-    assert.is(currentSnippet, 2);
+    assert.type(currentSnippet, "function");
 });
 test.run();
