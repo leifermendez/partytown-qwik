175f8c6df322ece91dbde18355748c1f0cd7016d
Leifer Mendez <leifer33@gmail.com>
2022-09-26 13:47:18 +0200

    Delete node_modules directory

--- node_modules/@builder.io/qwik/jsx-runtime.mjs
+++
@@ -1,9 +0,0 @@
-// packages/qwik/src/jsx-runtime/index.ts
-import { jsx, jsxs, jsxDEV, Fragment } from "@builder.io/qwik";
-export {
-  Fragment,
-  jsx,
-  jsxDEV,
-  jsxs
-};
-//# sourceMappingURL=jsx-runtime.mjs.map
